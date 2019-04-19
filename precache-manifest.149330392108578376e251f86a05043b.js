e22e31d5e31e598736d0c4e4748c4b0be201fa86
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 04:28:55 +0000

    Deploy whoisnegrello/react-hello-world to github.com/whoisnegrello/react-hello-world.git:master

---
+++ precache-manifest.149330392108578376e251f86a05043b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977703b8691bd58a3dc1",
+    "url": "./assets/css/home.css"
+  },
+  {
+    "revision": "977703b8691bd58a3dc1",
+    "url": "./assets/js/home.js"
+  },
+  {
+    "revision": "975589fe4148a5997839",
+    "url": "./assets/js/vendor.js"
+  },
+  {
+    "revision": "c123e810d6e73fdd44a0a0d90e6ad926",
+    "url": "index.html"
+  }
+]);
