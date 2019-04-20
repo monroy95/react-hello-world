6c8cceeb73e057b2ed7ba916e11e9fb7f45568b9
Lucio Negrello <lucionegrelloyyahoo.com.ar>
2019-04-19 23:36:41 -0300

    Add Service Worker from index.html.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
     render
 } from 'react-dom'
-import './utils/load-service-worker'
 import App from "./containers/App"
 
 render(<App /> , document.getElementById('app'));
